ad4c09b092475b98ed60874177aab605dad2caa6
PrasanthiDadi73 <prasanthidadi01@gmail.com>
2024-06-26 11:39:51 +0530

    SpringBoot with Jenkins

---
+++ src/test/java/spring/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package spring.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+//	Logger log= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+//
+//	@Test
+//	void contextLoads() {
+//		log.info("Test case executing");
+//		assertEquals(true,true);
+//	}
+
+}
